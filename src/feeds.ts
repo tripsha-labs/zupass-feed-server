1d246cc884178aa873590e1b0fa4c74066297f01
Rob Knight <mail@robknight.org.uk>
2023-09-26 18:09:27 +0200

    Add multiple products

--- src/feeds.ts
+++ src/feeds.ts
@@ -16,6 +16,10 @@ import { ZUPASS_PUBLIC_KEY } from "./main";
 EdDSAPCDPackage.init?.({});
 EdDSATicketPCDPackage.init?.({});
 
+const EVENT_ID = "32cb7908-aad7-4ee4-8eb7-49227f4b28e7";
+const REGULAR_PRODUCT_ID = "39ed8025-2980-4bfb-ae18-6a1b0970d5f4";
+const DAYPASS_PRODUCT_ID = "2c1c5aea-67be-4b54-87ab-427275e46c37";
+
 export const feedHost = new FeedHost(
   [
     {
@@ -46,7 +50,19 @@ export const feedHost = new FeedHost(
                   pcds: [
                     await issueTestPCD(
                       pcd.claim.emailAddress,
-                      pcd.claim.semaphoreId
+                      pcd.claim.semaphoreId,
+                      REGULAR_PRODUCT_ID
+                    )
+                  ],
+                  folder: "Testing",
+                  type: PCDActionType.ReplaceInFolder
+                } as ReplaceInFolderAction,
+                {
+                  pcds: [
+                    await issueTestPCD(
+                      pcd.claim.emailAddress,
+                      pcd.claim.semaphoreId,
+                      DAYPASS_PRODUCT_ID
                     )
                   ],
                   folder: "Testing",
@@ -66,7 +82,8 @@ export const feedHost = new FeedHost(
 
 async function issueTestPCD(
   emailAddress: string,
-  semaphoreId: string
+  semaphoreId: string,
+  productId: string
 ): Promise<SerializedPCD<EdDSATicketPCD>> {
   const ticketData = {
     attendeeName: "test name",
@@ -75,8 +92,8 @@ async function issueTestPCD(
     ticketName: "ticket",
     checkerEmail: "checker@test.com",
     ticketId: uuid(),
-    eventId: uuid(),
-    productId: uuid(),
+    eventId: EVENT_ID,
+    productId: productId,
     timestampConsumed: Date.now(),
     timestampSigned: Date.now(),
     attendeeSemaphoreId: semaphoreId,
